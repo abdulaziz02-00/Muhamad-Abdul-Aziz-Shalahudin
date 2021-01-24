f726f22e5e1a11af7a3da8902ab5d960bbc00fc6
Muhamad Abdul Aziz Shalahudin <77939525+abdulaziz02-00@users.noreply.github.com>
2021-01-25 04:57:37 +0700

    Add files via upload

---
+++ peminjaman.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Jan 2021 pada 22.44
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bdperpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `Nomor_Pinjam` varchar(6) NOT NULL,
+  `Tanggal_Pinjam` datetime NOT NULL,
+  `Total_Pinjam` int(11) NOT NULL,
+  `Nomor_Buku` varchar(6) NOT NULL,
+  `Judul` varchar(50) NOT NULL,
+  `Nomor_Anggota` varchar(6) NOT NULL,
+  `Nama_Anggota` varchar(50) NOT NULL,
+  `Kode_Petugas` varchar(6) NOT NULL,
+  `Nama_Petugas` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`Nomor_Pinjam`, `Tanggal_Pinjam`, `Total_Pinjam`, `Nomor_Buku`, `Judul`, `Nomor_Anggota`, `Nama_Anggota`, `Kode_Petugas`, `Nama_Petugas`) VALUES
+('333331', '2021-01-24 11:33:58', 2, '111112', 'Muhammad Al-Fatih', '000001', 'Maemunah', '222221', 'Eko'),
+('333332', '2021-01-23 11:40:13', 1, '111115', 'NDP HMI', '000002', 'Subardjo', '222222', 'Salas'),
+('333333', '2021-01-21 11:40:13', 1, '111116', 'Islam Dokterin dan Peradaban', '000003', 'Sutrisno', '222223', 'Ahmad'),
+('333334', '2021-01-23 11:44:08', 1, '111114', 'Revolusi Pemuda', '000004', 'Supriyatno', '222224', 'Adly');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
